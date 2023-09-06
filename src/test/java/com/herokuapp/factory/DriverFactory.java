fc7c737152c8778e304de151e1f0e5052ba183c5
Antonio Seraus <41804476+SerausAntonio@users.noreply.github.com>
2023-09-07 01:18:27 +0200

    timeout added

--- src/test/java/com/herokuapp/factory/DriverFactory.java
+++ src/test/java/com/herokuapp/factory/DriverFactory.java
@@ -4,12 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.time.Duration;
+
 public class DriverFactory {
 
     public WebDriver initializeDriver(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         return driver;
     }
 }
